09c2fb1e5b38cf2f2ae1cab6ca0eca20f9417788
Uday Verma <uday.karan@gmail.com>
2014-07-11 20:13:58 -0500

    Draw in passes

--- src-cljs/pre_k_adventures/core.cljs
+++ src-cljs/pre_k_adventures/core.cljs
@@ -1,7 +1,5 @@
 (ns pre-k-adventures.core
-  (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]
-            [cljs.core.match])
+  (:require [cljs.core.match])
   (:require-macros [cljs.core.match.macros :refer [match]]))
 
 (def image 
@@ -23,33 +21,54 @@
   ([ctx img sx sy sw sh dx dy dw dh]
    (.drawImage ctx img sx sy sw sh dx dy dw dh)))
 
-(def level ["^------$"
-            "< o    >"
-            "<      >"
-            "<      >"
-            "<  o   >"
-            "<      >"
-            "<      >"
-            "< o  o >"
-            "v++++++%"])
+(defn blit-cell!
+  "Blit a cell"
+  ([ctx img sx sy dx dy]
+   (blit! ctx img (* sx 64) (* sy 64) 64 64 (* dx 64) (* dy 64)))
+
+  ([ctx img sx sy sw sh dx dy]
+   (blit! ctx img (* sx 64) (* sy 64) (* sw 64) (* sh 64) (* dx 64) (* dy 64))))
+
+(def level ["^---------$"
+            "< o       >"
+            "<         >"
+            "<         >"
+            "<  o  l   >"
+            "<         >"
+            "<         >"
+            "< o  O    >"
+            "v+++++++++%"])
+
+(defn offset-by [e by]
+  (+ e (/ by 64)))
 
 (def symbols
-  { \^ #(blit! %1 %2 0 0 64 64 %3 %4)
-    \- #(blit! %1 %2 64 0 64 64 %3 %4)
-    \+ #(blit! %1 %2 64 128 64 64 %3 %4)
-    \< #(blit! %1 %2 0 64 64 64 %3 %4)
-    \> #(blit! %1 %2 128 64 64 64 %3 %4)
-    \  #(blit! %1 %2 64 64 64 64 %3 %4)
-    \% #(blit! %1 %2 128 128 64 64 %3 %4)
-    \v #(blit! %1 %2 0 128 64 64 %3 %4)
-    \$ #(blit! %1 %2 128 0 64 64 %3 %4)
+  { \^ #(blit-cell! %1 %2 0 0 %3 %4)
+    \- #(blit-cell! %1 %2 1 0 %3 %4)
+    \+ #(blit-cell! %1 %2 1 2 %3 %4)
+    \< #(blit-cell! %1 %2 0 1 %3 %4)
+    \> #(blit-cell! %1 %2 2 1 %3 %4)
+    \  #(blit-cell! %1 %2 1 1 %3 %4)
+    \% #(blit-cell! %1 %2 2 2 %3 %4)
+    \v #(blit-cell! %1 %2 0 2 %3 %4)
+    \$ #(blit-cell! %1 %2 2 0 %3 %4)
 
     \o (fn [ctx img x y]
-         (blit! ctx img 64 64 64 64 x y)
-         (blit! ctx img 128 576 64 64 (- x 8) (- y 16))
-         (blit! ctx img 192 576 64 64 (- x 20) (- y 10))
-         (blit! ctx img 0 576 64 64 x y)
-         (blit! ctx img 64 576 64 64 (+ 16 x) y))
+         (blit-cell! ctx img 2 9 (offset-by x 8) (offset-by y -16))
+         (blit-cell! ctx img 3 9 (offset-by x -20) (offset-by y -10))
+         (blit-cell! ctx img 0 9 x y)
+         (blit-cell! ctx img 1 9 (offset-by x 16) y))
+
+   \O (fn [ctx img x y]
+         (blit-cell! ctx img 3 9 (offset-by x 20) (offset-by y -40))
+         (blit-cell! ctx img 0 9 x (offset-by y -16))
+         (blit-cell! ctx img 2 9 (offset-by x 8) (offset-by y -8)))
+
+   ;; for multi-tile tiles always draw up, drawing down will be overritten by the next line
+   \l (fn [ctx img x y]
+        (blit-cell! ctx img 3 10 1 2 (offset-by x -20) (offset-by (- y 1) -4))
+        (blit-cell! ctx img 0 10 1 2 x (- y 1))
+        (blit-cell! ctx img 1 10 1 2 (offset-by x 16) (offset-by (- y 1) 10)))
    })
 
 
@@ -67,12 +86,24 @@
     (set! (.-fillStyle ctx) "#ffffff")
     (.fillRect ctx 0 0 w h)))
 
+(defn draw-pass
+  ([ctx level f default]
+   "Only draw cells for which f returns truthy, for cells where f is not truthy, default is drawn"
+   (doseq [[line row] (map list level (range))
+           [c i] (map list line (range))
+           c (if (f c) c default)]
+     (let [fdraw (symbols c)]
+       (fdraw ctx image i row))))
+
+  ([ctx level f]
+   "Only draw cells for which f returns truthy"
+   (draw-pass ctx level f nil)))
+
 (defn game [state ctx]
-  (clear! ctx)
-  (doseq [[line row] (map list level (range))
-          [c i] (map list line (range))]
-      (let [f (symbols c)]
-        (f ctx image (* 64 i) (* 64 row)))))
+  (let [bg-tiles #{ \  \+  \< \> \- \^ \$ \v \% }]
+    (clear! ctx)
+    (draw-pass ctx level bg-tiles \ )
+    (draw-pass ctx level (complement bg-tiles))))
 
 (defn get-context [el]
   (.getContext el "2d"))
--- src-cljs/pre_k_adventures/util.cljs
+++
@@ -1,4 +0,0 @@
-(ns pre-k-adventures.utils)
-
-(defn tile-offset [x y]
-  (str "-" (* x 64) "px -" (* y 64) "px"))
